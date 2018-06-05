45d7428207fbdcfaeb45ac22d2adb5de4e244a27
Andy <andyviar@gmail.com>
2018-06-04 20:45:30 -0500

    Update hello.cs

--- hello.cs
+++ hello.cs
@@ -3,8 +3,8 @@ using System;
 namespace HelloWorldApplication {
    class HelloWorld {
       static void Main(string[] args) {
-         /* test 1 */
-         Console.WriteLine("Hello World");
+         /* function body */
+         Console.WriteLine("'ello guvn'r");
          Console.ReadKey();
       }
    }
